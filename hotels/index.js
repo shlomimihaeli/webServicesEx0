28e14e912a3a7d67429773ad931c4cf21abf247c
shnizle <dizlle@gmail.com>
2016-04-06 23:50:02 +0200

    added event handeling

--- hotels/index.js
+++ hotels/index.js
@@ -1,20 +1,47 @@
-HotelService = function(){
+var events = require('events');
+
+function HotelService(){
+	
+	/**
+	 * call super 
+	 */
+	events.EventEmitter.call(this);
+	
+	this.hotels = {};
+	
 };
 
+/**
+ * inheritance 
+ */
+HotelService.prototype = Object.create(events.EventEmitter.prototype);
+
 HotelService.log = "";
+HotelService.event = {UP:"up", DOWN:"down"};
 
 HotelService.prototype.rankUp = function(hotelName){
 	
+	if(this.hotels[hotelName] == undefined) this.hotels[hotelName]= 1;
+	else this.hotels[hotelName]++;
+	
 	HotelService.log += "ranked up hotel: "+hotelName+"\n";
+	this.emit(HotelService.event.UP);
 	
 }
 
 HotelService.prototype.rankDown = function(hotelName){
 	
+	if(this.hotels[hotelName] == undefined) this.hotels[hotelName] = 0;
+	else if( this.hotels[hotelName] > 0 ) this.hotels[hotelName]--;
 	HotelService.log += "ranked down hotel: "+hotelName+"\n";
-	
+	this.emit(HotelService.event.DOWN);
 }
 
+HotelService.prototype.getHotels = function(){
+	
+	return this.hotels;
+	
+}
 HotelService.prototype.getLog = function(){
 	
 	return HotelService.log;
--- index.js
+++ index.js
@@ -1,14 +1,29 @@
 var http = require ('http');
+
 var hotelService = require('./hotels');
 
 http.createServer(function(req, res){
 
 	res.writeHead(200);
 	
+	hotelService.on("up", function(){
+		
+		res.write('rank up called\n');
+		res.write('total ranking: '+JSON.stringify(hotelService.getHotels())+"\n");
+	});
+	
+	hotelService.on("down", function(){
+		
+		res.write('rank down called'+"\n");
+		res.write('total ranking: '+JSON.stringify(hotelService.getHotels())+"\n");
+	});
+	
+	hotelService.rankUp("cramim");
+	hotelService.rankUp("cramim");
 	hotelService.rankUp("cramim");
 	hotelService.rankDown("cramim");
 	
-	res.write(hotelService.getLog());
+	
 	res.end();
 }).listen(3000);
 console.log('listening on port 3000');
